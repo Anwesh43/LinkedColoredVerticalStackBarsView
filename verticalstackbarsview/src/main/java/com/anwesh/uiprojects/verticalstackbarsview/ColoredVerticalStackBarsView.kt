56bc1e1e594233fa3fecc33263b14a4b9b97a065
Anwesh43 <anweshthecool0@gmail.com>
2020-06-27 19:04:10 +0530

    adding Canvas extension methods

--- verticalstackbarsview/src/main/java/com/anwesh/uiprojects/verticalstackbarsview/ColoredVerticalStackBarsView.kt
+++ verticalstackbarsview/src/main/java/com/anwesh/uiprojects/verticalstackbarsview/ColoredVerticalStackBarsView.kt
@@ -18,8 +18,34 @@ val scGap : Float = 0.02f
 val wFactor : Float = 3f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
+val bars : Int = 5
+val hFactor : Float = 2f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.sin(this * Math.PI).toFloat()
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawVerticalStackBar(i : Int, w : Float, h : Float, scale : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sfi : Float = sf.divideScale(i, bars)
+    val totalH = h / hFactor
+    val currH = totalH / bars
+    val y : Float = h - currH * i
+    val wSize : Float = w / wFactor
+    save()
+    translate(-wSize / 2 + (w / 2 + wSize / 2) * sfi, y - currH)
+    drawRect(RectF(-wSize / 2, 0f, wSize / 2, currH), paint)
+    restore()
+}
+
+fun Canvas.drawVSBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    save()
+    for (j in 0..(bars - 1)) {
+        drawVerticalStackBar(i, w, h, scale, paint)
+    }
+    restore()
+}
