0bc70bf83617df0067d988d548e5202f70b949ad
Anwesh43 <anweshthecool0@gmail.com>
2020-06-27 18:58:07 +0530

    adding libraries and constants

--- verticalstackbarsview/src/main/java/com/anwesh/uiprojects/verticalstackbarsview/ColoredVerticalStackBarsView.kt
+++ verticalstackbarsview/src/main/java/com/anwesh/uiprojects/verticalstackbarsview/ColoredVerticalStackBarsView.kt
@@ -3,3 +3,18 @@ package com.anwesh.uiprojects.verticalstackbarsview
 /**
  * Created by anweshmishra on 27/06/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val colors : Array<String> = arrayOf("", "", "", "", "")
+val scGap : Float = 0.02f
+val wFactor : Float = 3f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
