466b79621c9fc524ec59ddf0ca00c2dcc84ca669
Anwesh43 <anweshthecool0@gmail.com>
2020-06-27 19:14:47 +0530

    adding VSBNode class

--- verticalstackbarsview/src/main/java/com/anwesh/uiprojects/verticalstackbarsview/ColoredVerticalStackBarsView.kt
+++ verticalstackbarsview/src/main/java/com/anwesh/uiprojects/verticalstackbarsview/ColoredVerticalStackBarsView.kt
@@ -112,4 +112,45 @@ class ColorVertivalStackBarView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class VSBNode(var i : Int, val state : State = State()) {
+
+        private var next : VSBNode? = null
+        private var prev : VSBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = VSBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawVSBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : VSBNode {
+            var curr : VSBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
